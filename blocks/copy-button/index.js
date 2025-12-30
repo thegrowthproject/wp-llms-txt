d43768e14bdd33dcd845b0fdc1188339967811fc
Daniel Vacic <daniel@thegrowthproject.io>
2025-12-30 14:17:28 +1100

    fix: add stroke/fill attributes to block icons for outlined style

--- blocks/copy-button/index.js
+++ blocks/copy-button/index.js
@@ -30,12 +30,15 @@
 		el( 'path', { d: 'M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1' } )
 	);
 
-	// Block icon - matches WordPress button block icon
+	// Block icon - matches WordPress button block icon (outlined style)
 	const blockIcon = el( 'svg', {
 		xmlns: 'http://www.w3.org/2000/svg',
 		width: 24,
 		height: 24,
-		viewBox: '0 0 24 24'
+		viewBox: '0 0 24 24',
+		fill: 'none',
+		stroke: 'currentColor',
+		strokeWidth: 1.5
 	},
 		el( 'path', { d: 'M8 12.5h8M19 6.5H5a2 2 0 0 0-2 2v7a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7a2 2 0 0 0-2-2Z' } )
 	);
--- blocks/view-button/index.js
+++ blocks/view-button/index.js
@@ -33,12 +33,15 @@
 		el( 'polyline', { points: '10 9 9 9 8 9' } )
 	);
 
-	// Block icon - matches WordPress button block icon
+	// Block icon - matches WordPress button block icon (outlined style)
 	const blockIcon = el( 'svg', {
 		xmlns: 'http://www.w3.org/2000/svg',
 		width: 24,
 		height: 24,
-		viewBox: '0 0 24 24'
+		viewBox: '0 0 24 24',
+		fill: 'none',
+		stroke: 'currentColor',
+		strokeWidth: 1.5
 	},
 		el( 'path', { d: 'M8 12.5h8M19 6.5H5a2 2 0 0 0-2 2v7a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7a2 2 0 0 0-2-2Z' } )
 	);
